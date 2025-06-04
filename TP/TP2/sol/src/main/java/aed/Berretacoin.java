6052fc1227960bbe400ccc1fcdc7290d42d7e1c0
Jasson-01 <jason.davila001@gmail.com>
2025-06-04 00:17:36 -0300

    Complejidad
    Agregando la complejidad por defecto

--- TP/TP2/sol/src/main/java/aed/Berretacoin.java
+++ TP/TP2/sol/src/main/java/aed/Berretacoin.java
@@ -2,31 +2,31 @@ package aed;
 
 public class Berretacoin {
 
-    public Berretacoin(int n_usuarios){
+    public Berretacoin(int n_usuarios){ // O(P)
         throw new UnsupportedOperationException("Implementar!");
     }
 
-    public void agregarBloque(Transaccion[] transacciones){
+    public void agregarBloque(Transaccion[] transacciones){ // O(n_b * log(P))
         throw new UnsupportedOperationException("Implementar!");
     }
 
-    public Transaccion txMayorValorUltimoBloque(){
+    public Transaccion txMayorValorUltimoBloque(){ // O(1)
         throw new UnsupportedOperationException("Implementar!");
     }
 
-    public Transaccion[] txUltimoBloque(){
+    public Transaccion[] txUltimoBloque(){ // O(n_b)
         throw new UnsupportedOperationException("Implementar!");
     }
 
-    public int maximoTenedor(){
+    public int maximoTenedor(){ // O(1)
         throw new UnsupportedOperationException("Implementar!");
     }
 
-    public int montoMedioUltimoBloque(){
+    public int montoMedioUltimoBloque(){ // O(1)
         throw new UnsupportedOperationException("Implementar!");
     }
 
-    public void hackearTx(){
+    public void hackearTx(){ // O(log(n_b) + log(P))
         throw new UnsupportedOperationException("Implementar!");
     }
 }
